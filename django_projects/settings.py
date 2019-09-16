ba66e4f7c3eaf09d19903d47493bafa188b3d3b6
tetzng <ffffffffffffunk@gmail.com>
2019-09-16 22:25:22 +0900

    heroku11

--- django_projects/settings.py
+++ django_projects/settings.py
@@ -125,6 +125,12 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'index'
 
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
 DEBUG = False
 
 try:
